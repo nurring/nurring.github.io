9d2d0a51a5cddfca3b99ac2de0603228fd9e19a0
nurring <nurring@gmail.com>
2019-12-12 18:01:10 +0900

    Site updated at 2019-12-12 09:01:09 UTC

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "웹 기반 리조트 예약 시스템",
+        "excerpt":"개발 환경 eclipse JavaScript CSS Mysql Tomcat 개발 개요 웹 프로그래밍 언어를 이용하여 웹페이지 설계, 구현하였다. 룸타입/기간 단위로 리조트 숙박을 예약하고 후기 작성/수정/삭제할 수 있고, 관리자 로그인 기능을 추가하여 예약 수정 및 삭제, 공지 게시글 작성 권한을 부여하였다. 모바일 전용 웹페이지 구성하여 모바일 최적화 DB 설계 주요 사용 기능 구현...","categories": ["portfolios"],
+        "tags": ["Java","JPA","JavaScript","Mysql"],
+        "url": "https://nurring.github.io/portfolios/resort/",
+        "teaser":null},{
+        "title": "Spring, Hibernate를 적용한 사용자 관리 모듈 구현<br>- 기업 연계 프로젝트",
+        "excerpt":"GitHub Here: https://github.com/nurring/KOPOUcProject 개발 환경 Spring 4 Hibernate Ignite Mysql Tomcat 8.5 개발 개요 MVC 모델을 적용한 UC(Unified Communications) 사용자 관리 모듈을 구현하였다. 사용자와 연락처 엔티티를 매핑하였으며 Persistant API를 이용하여 SQL 명시를 최대한 줄이고, 1.2차 캐시를 적용하여 대량의 데이터 처리에 유연하도록 구현했다. DB 설계 주요 사용 기능 Maven 프로젝트에 의존성을...","categories": ["portfolios"],
+        "tags": ["Spring","Hibernate","Ignite","JPA"],
+        "url": "https://nurring.github.io/portfolios/ucmodule/",
+        "teaser":null},{
+        "title": "디바이스 리포트 & 실시간 모니터링 웹 프로그램<br>- 기업 연계 프로젝트",
+        "excerpt":"GitHub Here: https://github.com/nurring/KOPOgztls 개발 환경 Java Spring4 myBatis Mysql Tomcat9 개발 개요 IoT 기기의 데이터를 실시간으로 모니터링하고 통계와 함께 리포트하는 웹 프로그램을 구현하였다. 통계 그래프 구현을 위한 컬럼과 테이블 조인을 세부적으로 조정하기 위하여 myBatis를 이용하였으며, 실시간 데이터 감지 및 그래프 업데이트를 위해 ajax long polling을 적용하였다. DB 설계 주요 사용...","categories": ["portfolios"],
+        "tags": ["Spring","Mybatis"],
+        "url": "https://nurring.github.io/portfolios/devicemntr/",
+        "teaser":null}]
